239841d9cb2aeaa322b39672634b73b71ce15cae
PaulaMH <paulamorenohermoso99@gmail.com>
2022-04-07 23:49:17 +0200

    "Modificado un poquitín"

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,8 @@
 $("#nav__menu").click(function () {
-    $(".nav-showed").width(450);
-    $(".main").css("z-index", 300);
+    $(".nav-showed").width(450).css("z-index", 300);
+    $(".header__logo-container" > img).css("opacity", 1);
 });
 
 $(".btn__close").click(function () {
-    $(".nav-showed").width(0);
-    $(".main").css("z-index", 0);
+    $(".nav-showed").width(0).css("z-index", 0);
 })
