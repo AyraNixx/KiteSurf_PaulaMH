1b1f2703a52626d1986feac7562f6df6ebef98ba
PaulaMH <paulamorenohermoso99@gmail.com>
2022-04-21 13:04:37 +0200

    "Actualización del menú (ahora me gusta más como se ve)"

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,17 @@
-$("#nav__menu").click(function () {
-    $(".nav-showed").width(370).css("z-index", 300);
+$("#nav__menu").click(() => {
+    $(".nav-showed").css("z-index", 300).delay(40).queue(function() {
+        $(this).width(370);
+        $(this).dequeue();
+    })
 });
 
+/*Utilizamos queue junto con delay porque delay solo funciona con elementos en la cola*/
+/*Una cola, está formada por las funciones que esperan ser ejecutadas y con el método
+queue podemos modiciarla*/
 $(".btn__close").click(function () {
-    $(".nav-showed").width(0).css("z-index", 0);
-})
+        $(".nav-showed").width(0).delay(200).queue(function() {
+            $(this).css("z-index", 0);
+            /*Una vez que se realice la funcion, la eliminamos de la cola*/
+            $(this).dequeue();
+        });
+});
