958e311716923a1cefbe1274072e364bb6860ba3
PaulaMH <paulamorenohermoso99@gmail.com>
2022-04-21 13:07:59 +0200

    "Pequeñas pruebas (nada terminado aún)"

--- js/pie.js
+++ js/pie.js
@@ -1,3 +1,16 @@
 $(".footer__slideup").click(function () {
     $(".footer").css("height", 200)
+
+    $(".footer__slideup").attr("class", "fa-solid fa-circle-chevron-down footer__down");
 });
+
+
+$(".footer__down").click(function () {
+alert("ali")
+
+    $(".footer__down").attr("class", "fa-solid fa-circle-chevron-up footer__slideup");
+
+    $(".footer").css("height", "auto");
+
+});
+
